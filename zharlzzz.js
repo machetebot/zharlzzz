71f9e8da5f5983740b36cedcd98a57b9d7011e41
ֶָ֢𐍐հᨡ֟፝ؖ۬𐑾꫶ࠋꜟৎֶֶָָ֢֢ ‎ <38163345+machetebot@users.noreply.github.com>
2024-12-10 20:39:57 +0800

    Update zharlzzz.js

--- zharlzzz.js
+++ zharlzzz.js
@@ -1,5 +1,7 @@
-const images = ['https://fonts.gstatic.com/s/e/notoemoji/latest/1f970/512.gif', 'https://fonts.gstatic.com/s/e/notoemoji/latest/263a_fe0f/512.gif', 'https://fonts.gstatic.com/s/e/notoemoji/latest/1f60d/512.gif', 
-'      https://fonts.gstatic.com/s/e/notoemoji/latest/1f61d/512.gif',
+const images = ['https://fonts.gstatic.com/s/e/notoemoji/latest/1f970/512.gif', 
+'https://fonts.gstatic.com/s/e/notoemoji/latest/263a_fe0f/512.gif', 
+'https://fonts.gstatic.com/s/e/notoemoji/latest/1f60d/512.gif', 
+'https://fonts.gstatic.com/s/e/notoemoji/latest/1f61d/512.gif',
 'https://fonts.gstatic.com/s/e/notoemoji/latest/1f61c/512.gif',
 'https://fonts.gstatic.com/s/e/notoemoji/latest/2764_fe0f_200d_1f525/512.gif',
 'https://fonts.gstatic.com/s/e/notoemoji/latest/1f496/512.gif',
@@ -8,20 +10,15 @@ const images = ['https://fonts.gstatic.com/s/e/notoemoji/latest/1f970/512.gif', 
 'https://fonts.gstatic.com/s/e/notoemoji/latest/1f498/512.gif',
 'https://fonts.gstatic.com/s/e/notoemoji/latest/1f495/512.gif',
 'https://fonts.gstatic.com/s/e/notoemoji/latest/1f618/512.gif'];
-
 const rand = Math.floor(Math.random()*images.length);
 const src = images[rand];
-
 console.log(rand, src);
-
 const img = document.createElement('IMG');
 img.src = src;
-
 img.height = "100";
 img.width = "100";
 img.style.position = "fixed";
 img.style.right = "150px";
 img.style.bottom = "90px";
 img.style.zIndex = "-1x";
-
 document.body.appendChild(img); 
